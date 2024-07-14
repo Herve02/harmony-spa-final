5d7974a99d26319ea014f77b215424ebb39a4606
gyhaan <g.yhaan@alustudent.com>
2024-07-14 17:06:22 +0200

    finished both pages

--- statics/js/menu.js
+++ statics/js/menu.js
@@ -4,20 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const navigation = document.querySelector("ul");
   const backToTop = document.querySelector(".back-to-top");
 
-  menu.addEventListener("click", function (e) {
-    navigation.classList.toggle("mobile-nav");
-  });
-
-  close.addEventListener("click", function (e) {
-    navigation.classList.toggle("mobile-nav");
-  });
-
-  navigation.addEventListener("click", function (e) {
-    const clicked = e.target.closest("a");
-    if (!clicked) return;
-    navigation.classList.toggle("mobile-nav");
-  });
-
   window.addEventListener("scroll", function () {
     if (window.scrollY < 200) {
       backToTop.style.display = "none";
